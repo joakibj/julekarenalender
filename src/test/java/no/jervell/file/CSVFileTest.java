f16832b4c19cfe078ec83f8924fe52dd04c7da51
Joakim Bjornstad <joakibj@gmail.com>
2012-11-26 14:14:42 +0100

    Endret test til å ta høyde for system spesifikke linjeskift

--- src/test/java/no/jervell/file/CSVFileTest.java
+++ src/test/java/no/jervell/file/CSVFileTest.java
@@ -18,12 +18,14 @@ public class CSVFileTest {
     public TemporaryFolder tmpFolder = new TemporaryFolder();
     private CSVFile dataSource;
 
+    private String newLine = System.getProperty("line.separator");
+
     @Test
     public void shouldReadDefaultCSVFile() throws IOException {
-        String csv = "Name;Picture;Day;Note\n" +
-                "Ole;ole.jpg;1;Borte\n" +
-                "Dole;dole.jpg;2;\n" +
-                "Doffen;doffen.jpg;3;\n" +
+        String csv = "Name;Picture;Day;Note" + newLine +
+                "Ole;ole.jpg;1;Borte" + newLine +
+                "Dole;dole.jpg;2;" + newLine +
+                "Doffen;doffen.jpg;3;" + newLine +
                 "Donald;donald.jpg;";
         File file = createTemporaryFileWithContents("default.csv", csv);
         dataSource = new CSVFile(file, true);
@@ -55,11 +57,56 @@ public class CSVFileTest {
         assertThat(dataSource.get(3, "Day", -99), is(-99));
     }
 
+    @Test
+    public void shouldReadAndSaveCSVFileAfterChanges() throws IOException {
+        String csv = "Name;Picture;Day;Note" + newLine +
+                "Ole;ole.jpg;1;Borte" + newLine +
+                "Dole;dole.jpg;2;" + newLine +
+                "Doffen;doffen.jpg;3;" + newLine +
+                "Donald;donald.jpg;";
+        File file = createTemporaryFileWithContents("default.csv", csv);
+        dataSource = new CSVFile(file, true);
+
+        dataSource.set(3, "Name", "Dolly");
+        dataSource.set(3, "Picture", "dolly.jpg");
+        dataSource.set(3, "Day", "26");
+
+        dataSource.save();
+
+        String expected = "Name;Picture;Day;Note" + newLine +
+                "Ole;ole.jpg;1;Borte" + newLine +
+                "Dole;dole.jpg;2" + newLine +
+                "Doffen;doffen.jpg;3" + newLine +
+                "Dolly;dolly.jpg;26";
+
+        String actual = readFileToString(file);
+        assertThat(actual, is(expected));
+    }
+
     private File createTemporaryFileWithContents(String filename, String contents) throws IOException {
         File file = tmpFolder.newFile(filename);
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
         bufferedWriter.write(contents);
         bufferedWriter.close();
         return file;
+    }
+
+    private String readFileToString(File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringWriter sw = new StringWriter();
+            char[] buffer = new char[1024 * 4];
+            int n = 0;
+            while (-1 != (n = reader.read(buffer))) {
+                sw.write(buffer, 0, n);
+            }
+            String readString = sw.toString();
+            reader.close();
+            return readString;
+        } catch (FileNotFoundException e) {
+            return e.toString();
+        } catch (IOException e) {
+            return e.toString();
+        }
     }
 }
