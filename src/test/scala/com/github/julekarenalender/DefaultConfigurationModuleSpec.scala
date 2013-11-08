aa982ba3debc0311841c332e57c8deb9b3e50b0b
Joakim Bjørnstad <joakibj@gmail.com>
2013-11-08 14:53:24 +0100

    Test placeholder

---
+++ src/test/scala/com/github/julekarenalender/DefaultConfigurationModuleSpec.scala
@@ -0,0 +1,8 @@
+package com.github.julekarenalender
+
+import org.scalatest._
+
+class DefaultConfigurationModuleSpec  extends FlatSpec with ShouldMatchers with BeforeAndAfter {
+  val configModule = new DefaultConfigurationModule
+  
+}
